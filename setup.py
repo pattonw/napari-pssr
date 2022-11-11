2b1d538f32cb378c9c3ecf4ee05e39300ff0518f
pattonw <pattonw@hhmi.org>
2022-11-11 10:22:18 -0800

    add dependencies to the "install_requires"

--- setup.py
+++ setup.py
@@ -2,4 +2,11 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-setup()
+setup(install_requires=[
+    "numpy",
+    "zarr",
+    "magicgui",
+    "bioimageio.core",
+    "gunpowder @ git+https://github.com/funkey/gunpowder.git@patch-1.2.3",
+    "matplotlib",
+])
