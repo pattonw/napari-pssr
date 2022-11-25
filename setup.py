6feb8dbcc2b69bae82b652c20ebb5066546d6c5f
pattonw <pattonw@hhmi.org>
2022-11-24 17:18:15 -0800

    update install_requires in setup.py and setup.cfg

--- setup.py
+++ setup.py
@@ -2,13 +2,4 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-setup(install_requires=[
-    "numpy",
-    "zarr",
-    "magicgui",
-    "bioimageio.core",
-    "gunpowder",
-    "matplotlib",
-    "torch",
-    "napari",
-])
+setup()
